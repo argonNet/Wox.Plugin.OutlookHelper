8dfc73aa5f5587a14ae080f9f29b4f45ab852da8
argon <sebastien.schoepfer@gmail.com>
2019-04-07 22:29:03 +0200

    Creating test project

--- Wox.Plugin.OutlookHelper/Main.cs
+++ Wox.Plugin.OutlookHelper/Main.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+
 using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace Wox.Plugin.OutlookHelper
@@ -12,10 +13,21 @@ namespace Wox.Plugin.OutlookHelper
 
         public void Init(PluginInitContext context)
         {
+            System.Diagnostics.Debugger.Launch();
+
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText("c:\\temp\\test.txt"))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }	
+
             this.outlookApp = new Outlook.Application();
 
             this.functions = new Dictionary<char, BaseOutlookFunction>();
             this.functions.Add('e', new CreateEmail(outlookApp)); 
+
         }
 
         public List<Result> Query(Query query)
--- Wox.Plugin.OutlookHelper/OutlookFunctions/BaseOutlookFunction.cs
+++ Wox.Plugin.OutlookHelper/OutlookFunctions/BaseOutlookFunction.cs
@@ -1,17 +1,18 @@
 using System.Collections.Generic;
+
 using Wox.Plugin;
 using Outlook = Microsoft.Office.Interop.Outlook;
 
 public abstract class BaseOutlookFunction {
-    
-    protected const string DefaultImage= "Images\\outlook.png"; 
-    protected Outlook.Application outlookApp;
-    
 
+
+    protected const string DefaultImage= "Images\\outlook.png"; 
+
+    protected Outlook.Application outlookApp;
 
     public BaseOutlookFunction(Outlook.Application outlookApp){
         this.outlookApp = outlookApp;
-    } 
+    }
 
     public abstract List<Result> Execute(string subQuery);
 
